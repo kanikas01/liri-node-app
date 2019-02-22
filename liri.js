a912ff7e0636fd7739e87e5b5e0ae69ef0a11255
kanikas01 <larvakurt@yahoo.com>
2019-02-22 08:15:18 -0800

    Add concert-this functionality, refine spotify-this-song and do-what-it-says

--- liri.js
+++ liri.js
@@ -39,7 +39,39 @@ function main(action, userQuery) {
 }
 
 function concertThis(queryString) {
+  // Set query string
+  var query = "https://rest.bandsintown.com/artists/" + queryString + "/events?app_id=codingbootcamp";
 
+  // Run get request
+  axios.get(query)
+    .then(function (response) {
+      var venue, city, region, country, location, date;
+      console.log('\n-------------------\n');
+      for (var i = 0; i < response.data.length; i++) {
+        // Assign json values to variables
+        venue = response.data[i].venue.name;
+        city = response.data[i].venue.city;
+        region = response.data[i].venue.region;
+        country = response.data[i].venue.country;
+        date = response.data[i].datetime.split('T');
+
+        // Set location 
+        if (region) {
+          location = city + ', ' + region + ', ' + country;
+        } else {
+          location = city + ', ' + country;
+        }
+
+        // Print output
+        console.log("Venue: " + venue);
+        console.log("Location: " + location);
+        console.log("Date: " + moment(date[0], 'YYYY-MM-DD').format('MM/DD/YYYY'));
+        console.log('\n-------------------\n');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 // Query Spotify API for information about a song
@@ -58,14 +90,22 @@ function spotifyThisSong(queryString) {
 
   // Execute Spotify query
   spotifyQuery.then(function (data) {
+    var artist, song, preview, album;
     console.log('\n-------------------\n');
     for (var i = 0; i < data.tracks.items.length; i++) {
       // If song title is an exact match, print results
       if (data.tracks.items[i].name.toLowerCase() === queryString.toLowerCase()) {
-        console.log("Artist: " + data.tracks.items[i].artists[0].name);
-        console.log("Song: " + data.tracks.items[i].name);
-        console.log("Preview: " + data.tracks.items[i].external_urls.spotify);
-        console.log("Album: " + data.tracks.items[i].album.name);
+        // Assign json values to variables
+        artist = data.tracks.items[i].artists[0].name;
+        song = data.tracks.items[i].name;
+        preview = data.tracks.items[i].external_urls.spotify;
+        album = data.tracks.items[i].album.name;
+
+        // Print output
+        console.log("Artist: " + artist);
+        console.log("Song: " + song);
+        console.log("Preview: " + preview);
+        console.log("Album: " + album);
         console.log('\n-------------------\n');
       }
     }
@@ -77,6 +117,10 @@ function spotifyThisSong(queryString) {
 
 function movieThis(queryString) {
   // console.log(JSON.stringify(data, null, 2));
+  
+  // Set query string
+  // Run get request
+  // Print output
 }
 
 // Execute instructions in instructionsFile
@@ -88,17 +132,17 @@ function doWhatItSays() {
     }
 
     // Parse file content into variables
-    var [action, queryString] = data.split(",");
-    queryString = queryString.replace(/"/g, '').trim();
+    var [fileAction, fileQueryString] = data.split(",");
+    fileQueryString = fileQueryString.replace(/"/g, '').trim();
 
     // Abort execution if infinite loop is detected
-    if (action === 'do-what-it-says') {
+    if (fileAction === 'do-what-it-says') {
       console.log("FATAL: Infinite loop detected - aborting script\n");
       return;
     }
     
     // Call main function with new parameters
-    main(action, queryString);
+    main(fileAction, fileQueryString);
   });
 }
 
